e9c4991f7ed41ca106e40b0b7cf3e5988d627451
IgnatevD <ignatevds@gmail.com>
2021-11-15 20:03:02 +0200

    HTML and  SCSS HomePage

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,11 +1,29 @@
 import React from 'react';
-
+import styles from './HomePage.module.scss';
 import Container from '../../components/_shared/Container/Container';
 
 export default function HomePage() {
   return (
     <Container>
-      <div></div>
+      <div>
+      <h1 className={styles.title}>
+          “Regression testing. What is it? If the system compiles, that's good,
+          if it boots, that's great!”
+        </h1>
+
+        <p className={styles.nameTest}>Linus Torvalds</p>
+        <p className={styles.descriptionTest}>
+          Linux kernel creator, hacker, 1969
+        </p>
+        <div className={styles.conteinerTest}>
+          <div className={styles.blokTema}>
+            <p className={styles.text}>QA technical training</p>
+          </div>
+          <div className={styles.blokTemaTest}>
+            <p className={styles.text}>QA technical training</p>
+          </div>
+        </div>
+      </div>
     </Container>
   );
 }
