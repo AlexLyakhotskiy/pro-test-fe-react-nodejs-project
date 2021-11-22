df74f5a9eda4073d86238edd9e348c77bcbde870
Alexander Lyakhotskiy <alexlyakhotskiy@gmail.com>
2021-11-22 20:19:29 +0200

    merged branch

--- src/utils/apiServices.js
+++ src/utils/apiServices.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://agile-stream-56061.herokuapp.com/';
+axios.defaults.baseURL = 'http://localhost:8080/';
 
 export const apiToken = {
   set(token) {
